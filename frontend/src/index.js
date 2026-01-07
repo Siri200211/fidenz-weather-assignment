66c50dac56bf4a1b4fad843cd35c804db0229044
Siri200211 <it22325396@my.sliit.lk>
2026-01-08 04:45:06 +0530

    auth0 login issue access denied issue resolved

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-<Auth0Provider
-  domain="YOUR_DOMAIN"
-  clientId="YOUR_CLIENT_ID"
-  authorizationParams={{
-    redirect_uri: window.location.origin,
-    audience: "https://weather-api"
-  }}
->
-  <App />
-</Auth0Provider>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain= "dev-oucy02cp1k7f6sfd.us.auth0.com"
+      clientId= "jhHU7KqOtlxdxlyQxG5NXpXegrDndZOE"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://weather-api"
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
